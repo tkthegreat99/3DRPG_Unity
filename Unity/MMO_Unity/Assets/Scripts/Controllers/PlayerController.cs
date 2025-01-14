2a5f18cb76a71ef6d58b3e86b3e50aa97539c0c2
taegangyun <taegangyun@gmail.com>
2025-01-14 16:11:06 +0900

    Mouse Cursor

--- Unity/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ Unity/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -9,12 +9,23 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     PlayerStat _stat;
-
-    //bool _moveToDest = false;
     Vector3 _destpos;
-    
+    Texture2D _attackicon;
+    Texture2D _handicon;
+
+    enum CursorType
+    {
+        None,
+        Attack,
+        Hand,
+    }
+
+    CursorType _cursorType = CursorType.None;
+
     void Start()
     {
+        _attackicon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Attack");
+        _handicon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Hand");
         _stat = gameObject.GetComponent<PlayerStat>();
 
         Managers.Input.MouseAction -= OnMouseClicked; //처음에 빼주는 이유은 중복 구독신청 될까봐.
@@ -56,9 +67,9 @@ public class PlayerController : MonoBehaviour
 
             Debug.DrawRay(transform.position + Vector3.up * 0.5f, dir.normalized, Color.red);
 
-
+            int _mask = (1 << (int)Define.Layer.Blocking) | (1 << (int)Define.Layer.Monster);
             //LayerMask로 Blocking을 빌딩 등에 적용시키고, RayCast를 통해 플레이어의 방향 앞에 Blocking 이 있을 경우 Idle로 바꾸게.
-            if(Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Blocking")))
+            if (Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, _mask))
             {
                 _state = PlayerState.Idle;
                 return;
@@ -91,6 +102,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        UpdateMouseCursor();
 
         switch(_state)
         {
@@ -105,6 +117,38 @@ public class PlayerController : MonoBehaviour
                 break;
         }        
     }
+
+    void UpdateMouseCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, _mask))
+        {
+            if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+                if (_cursorType != CursorType.Attack)
+                {
+                    Cursor.SetCursor(_attackicon, new Vector2(_attackicon.width / 5, 0), CursorMode.Auto);
+                    _cursorType = CursorType.Attack;
+                }
+                // 커서 모양이 칼 모양인데 왼쪽 위로부터 칼 끝이 1/5 정도 밀려나있어서 보정.
+            }
+            else
+            {
+                if(_cursorType != CursorType.Hand)
+                {
+                    Cursor.SetCursor(_handicon, new Vector2(_handicon.width / 3, 0), CursorMode.Auto);
+                    _cursorType = CursorType.Hand;
+                }
+                
+            }
+        }
+    }
+
+
+
+
     //Ground와 Monster를 Layermask
     int _mask = (1 << (int)Define.Layer.Ground) | (1 << (int)Define.Layer.Monster);
     void OnMouseClicked(Define.MouseEvent evt)
