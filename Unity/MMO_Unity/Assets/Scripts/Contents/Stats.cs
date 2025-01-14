f914a6460fb350802ff28656ca720dcbaa3bffce
taegangyun <taegangyun@gmail.com>
2025-01-14 15:26:13 +0900

    Contents -> stats, Playerstat

--- Unity/MMO_Unity/Assets/Scripts/Contents/Stats.cs
+++ Unity/MMO_Unity/Assets/Scripts/Contents/Stats.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    int _level;
+    [SerializeField]
+    int _hp;
+    [SerializeField]
+    int _maxHp;
+    [SerializeField]
+    int _attack;
+    [SerializeField]
+    int _defense;
+    [SerializeField]
+    float _movespeed;
 
-    
-    void Update()
-    {
-        
-    }
+    public int Level { get { return _level; } set { _level = value; } }
+    public int HP { get { return _hp; } set { _hp = value; } }
+    public int MaxHp { get { return _maxHp; } set { _maxHp = value; } }
+    public int Attack { get { return _attack; } set { _attack = value; } }
+    public int Defense { get { return _defense; } set { _defense = value; } }
+    public float Speed { get { return _movespeed; } set { _movespeed = value; } }
 }
